86d993fe8032d7cd1cc1915a0cbbdaa2901510ad
saanamarika <saana.m.sieppi@gmail.com>
2017-10-06 15:27:59 +0300

    Task-3 ready

---
+++ Task-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ohjelma laskee kertoman. Anna luku, jonka kertoma lasketaan: ");
+            String userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            int x = 0;
+            int y = 0;
+            if (number > 0)
+            {
+                do
+                {
+                    x++;
+                    y += x;
+                } while (x < number);
+
+                Console.WriteLine("Syötit: " + number);
+                Console.WriteLine($"Vastaus: {y}");
+            }
+            else
+            {
+                Console.WriteLine("Maarittelematon");
+            }
+
+            Console.WriteLine("Kiitos!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task-3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ohjelma antaa parittomien ja parillisten yhteissumman, syötä luku: ");
+            String userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            int x = 0;
+            int y = 0;
+            int i = 0;
+            while (number >= i)
+            {
+                if (i % 2 == 0)
+                {
+                   x += i;
+                }
+                else
+                {
+                   y += i;
+                }
+                   i++;
+                }
+            Console.WriteLine("Syötit: " + number);
+            Console.WriteLine($"Vastaus: Parilliset={x} Parittomat={y}");
+            Console.WriteLine("Kiitos!");
+            Console.ReadKey();
+        }
+
+
+    }
+}
