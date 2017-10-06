f71e81d81c9c6d63c942562021fb516fa22ec653
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-06 21:01:55 +0300

    progress

--- Silmukka/Program.cs
+++ Silmukka/Program.cs
@@ -10,6 +10,7 @@ namespace Silmukka
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.Write("Ohjelma laskee kertoman. Anna luku, jonka kertoma lasketaan: ");
             string userInput = Console.ReadLine();
             int number = int.Parse(userInput);
@@ -22,13 +23,14 @@ namespace Silmukka
                 {
                     i++;
                     f *= i;
+                    //f = f * i;
                 } while (i < number);
-                Console.WriteLine("Syotit: " + number);
+                Console.WriteLine("Syötit: " + number);
                 Console.WriteLine($"Vastaus: {f}");
             }
             else
             {
-                Console.WriteLine("Maarittelematon");
+                Console.WriteLine("Määrittelemätön");
             }
             Console.WriteLine("Kiitos!");
             Console.ReadKey();
