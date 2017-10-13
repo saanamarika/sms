ede464d7983b9fa7e389a00d466e23d64ec315e0
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-13 08:35:33 +0300

    Loop ready

--- Task-4/Program.cs
+++ Task-4/Program.cs
@@ -7,10 +7,29 @@ namespace Task_4
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Ohjelma antaa negatiivisten lukujen summan. Parillinen ja Pariton. Syötä luku: ");
+            Console.Write("Ohjelma antaa negatiivisten lukujen summan. Parillinen ja Pariton. Syötä luku: ");
             string userInput = Console.ReadLine();
             int number = int.Parse(userInput);
 
+            int x = 0, y = 0, i = 0, k=1;
+            if (number < 0)
+                k = -1;
+            while (number * k >= i)
+            {
+                if (i % 2 == 0)
+                {
+                    x += i;
+                }
+                else
+                {
+                    y += i;
+                }
+                i++;
+            }
+            Console.WriteLine("Syötit: " + number);
+            Console.WriteLine($"Vastaus: Parilliset={x*k} Parittomat={y*k}");
+            Console.WriteLine("Kiitos!");
+            Console.ReadKey();
 
         }
     }
