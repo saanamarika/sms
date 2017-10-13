17078242d42ed7f33382bdc1ce87b618acd7b1c8
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-13 08:56:10 +0300

    loop ready

---
+++ Task-4.0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_4._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Ohjelma laskee kertoman. Anna luku, jonka kertoma lasketaan: ");
+            String userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            int x = 0;
+            int y = 0;
+            int k = 1;
+            if (number < 0)
+            {
+                k = -1;
+            }
+            do
+            {
+                x++;
+                y += x;
+            } while (x < number*k);
+
+            Console.WriteLine("Syötit: " + number);
+            Console.WriteLine($"Vastaus: {y*k}");
+
+            Console.WriteLine("Kiitos!");
+            Console.ReadKey();
+        }
+    }
+}
